a5017fb28d619956804319e07620810a365bf1fb
Yehiyam Livneh <yehiyam@gmail.com>
2022-01-19 15:50:11 +0200

    ff

--- scripts/startMongo.sh
+++ scripts/startMongo.sh
@@ -4,7 +4,7 @@ openssl rand -base64 756 > $KEY_FILE
 chown 999 $KEY_FILE
 chmod 400 $KEY_FILE
 docker rm -f mongodb
-docker run -d -it --name mongodb \
+docker run -d --name mongodb \
       -p 27017:27017 \
       -e MONGO_INITDB_ROOT_USERNAME=tester \
       -e MONGO_INITDB_ROOT_PASSWORD=password \
@@ -12,7 +12,7 @@ docker run -d -it --name mongodb \
       -v $KEY_FILE:$KEY_FILE \
       mongo:4.4.1-bionic --replSet rs0 --keyFile $KEY_FILE
 
-INIT=$(docker exec -it mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
+INIT=$(docker exec mongodb sh -c 'echo "rs.initiate().ok || rs.status().ok"|mongo -u tester -p password --quiet' | tr -d '\r')
 COUNT=1
 MAX_RETRY=10
 while [ "$INIT" != "1" ]; do
