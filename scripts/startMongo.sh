4d565c4d52ab4b767cde1390888495def67dbdeb
Yehiyam Livneh <yehiyam@gmail.com>
2022-01-19 15:45:08 +0200

    fix

--- scripts/startMongo.sh
+++ scripts/startMongo.sh
@@ -4,7 +4,7 @@ openssl rand -base64 756 > $KEY_FILE
 chown 999 $KEY_FILE
 chmod 400 $KEY_FILE
 docker rm -f mongodb
-docker run -d --name mongodb \
+docker run -d -it --name mongodb \
       -p 27017:27017 \
       -e MONGO_INITDB_ROOT_USERNAME=tester \
       -e MONGO_INITDB_ROOT_PASSWORD=password \
